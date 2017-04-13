d1f13f49a9d3d9e3e3461f52c1084d0f263524a1
Sosuke Suzuki <aosukeke@gmail.com>
2017-04-14 01:37:42 +0900

    refactored moveFolder

--- browser/main/lib/dataApi/moveFolder.js
+++ browser/main/lib/dataApi/moveFolder.js
@@ -6,7 +6,6 @@ const CSON = require('@rokt33r/season')
 /**
  * @param {String} storageKey
  * @param {Array} folders
-
  * @return {Object}
  * ```
  * {
@@ -18,25 +17,26 @@ const CSON = require('@rokt33r/season')
 function moveFolder (storageKey, folders) {
   let rawStorages
   let targetStorage
-  try {
-    rawStorages = JSON.parse(localStorage.getItem('storages'))
-    if (!_.isArray(rawStorages)) throw new Error('Target storage doesn\'t exist.')
+  let movedStorage
+  return new Promise((resolve, reject) => {
+    try {
+      rawStorages = JSON.parse(localStorage.getItem('storages'))
+      if (!_.isArray(rawStorages)) throw new Error('Target storage doesn\'t exist.')
 
-    targetStorage = _.find(rawStorages, {key: storageKey})
-    if (targetStorage == null) throw new Error('Target storage doesn\'t exist.')
-  } catch (e) {
-    return Promise.reject(e)
-  }
+      targetStorage = _.find(rawStorages, {key: storageKey})
+      if (targetStorage == null) throw new Error('Target storage doesn\'t exist.')
 
-  return resolveStorageData(targetStorage)
-    .then(function moveFolder (storage) {
-      storage.folders = folders
+      resolveStorageData(targetStorage)
+        .then((storage) => {
+          storage.folders = folders
+          movedStorage = storage
+          CSON.writeFileSync(path.join(storage.path, 'boostnote.json'), _.pick(storage, ['folders', 'version']))
 
-      CSON.writeFileSync(path.join(storage.path, 'boostnote.json'), _.pick(storage, ['folders', 'version']))
-
-      return {
-        storage
-      }
-    })
+        })
+      resolve(movedStorage)
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
 module.exports = moveFolder
