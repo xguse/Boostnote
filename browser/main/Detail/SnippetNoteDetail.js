a556fef2b07d3f3b199abc555640a13706c1d5df
Sosuke Suzuki <aosukeke@gmail.com>
2017-08-31 17:32:54 +0900

    Implement new EDIT_NOTE event for snippet

--- browser/main/Detail/SnippetNoteDetail.js
+++ browser/main/Detail/SnippetNoteDetail.js
@@ -60,6 +60,7 @@ class SnippetNoteDetail extends React.Component {
   componentWillReceiveProps (nextProps) {
     if (nextProps.note.key !== this.props.note.key && !this.isMovingNote) {
       if (this.saveQueue != null) this.saveNow()
+      if (this.sendEventQueue != null) this.sendEditnoteEvent()
       let nextNote = Object.assign({
         description: ''
       }, nextProps.note, {
@@ -80,6 +81,7 @@ class SnippetNoteDetail extends React.Component {
 
   componentWillUnmount () {
     if (this.saveQueue != null) this.saveNow()
+    if (this.sendEventQueue != null) this.sendEditnoteEvent()
   }
 
   handleChange (e) {
@@ -99,6 +101,12 @@ class SnippetNoteDetail extends React.Component {
 
   save () {
     clearTimeout(this.saveQueue)
+    clearTimeout(this.sendEventQueue)
+
+    this.sendEventQueue = setTimeout(() => {
+      this.sendEditnoteEvent()
+    }, 3000)
+
     this.saveQueue = setTimeout(() => {
       this.saveNow()
     }, 1000)
@@ -117,6 +125,12 @@ class SnippetNoteDetail extends React.Component {
           note: note
         })
       })
+  }
+
+  sendEditnoteEvent () {
+    clearTimeout(this.saveQueue)
+    this.sendEventQueue = null
+    AwsMobileAnalyticsConfig.recordDynamicCustomEvent('EDIT_NOTE')
   }
 
   handleFolderChange (e) {
